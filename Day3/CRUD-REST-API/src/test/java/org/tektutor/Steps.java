91e3449bbb7f9f88b361e052ba5c97cb429bc9b9
Jeganathan Swaminathan <mail2jegan@gmail.com>
2019-04-17 01:25:32 -0700

    Added Day3 Lab exercises.

--- Day3/CRUD-REST-API/src/test/java/org/tektutor/Steps.java
+++ Day3/CRUD-REST-API/src/test/java/org/tektutor/Steps.java
@@ -1,6 +1,6 @@
 package org.tektutor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -24,24 +24,78 @@ public class Steps {
     public Steps() {
     }
 
-    @Given("there is a REST API that supports retrieve all training records")
-    public void there_is_a_REST_API_that_supports_retrieve_all_training_records() {
-        endpointURL = "http://localhost:8080/rest/trainings";
-
-        RestAssured.baseURI = endpointURL;
-		this.request = RestAssured.given();
-		this.request.header( "Content-Type", "application/json" );
+    @Given("there is a REST API that supports retrieve all training records at {string}")
+    public void there_is_a_REST_API_that_supports_retrieve_all_training_records_at(String url) {
+        endpointURL = url;
     }
 
-    @When("I invoked the url")
-    public void i_invoked_the_url() {
-		this.response = request.get();
-		this.actualResponseCode = this.response.getStatusCode();
+    @Given("there is a REST API that supports retrieving a specific training record at {string}")
+    public void there_is_a_REST_API_that_supports_retrieving_a_specific_training_record_at(String url) {
+      endpointURL = url;
+    }
+    
+    @When("I invoked the REST endpoint with GET method")
+    public void i_invoked_the_REST_endpoint() {
+      RestAssured.baseURI = endpointURL;
+      this.request = RestAssured.given();
+      this.request.header( "Content-Type", "application/json" );
+      this.response = request.get();
+    }
+    
+    @Then("I expect the response code as {string}")
+    public void i_expect_the_response_code_as(String strExpectedResponseCode) {  
+      actualResponseCode = response.getStatusCode();
+      int expectedResponseCode  = Integer.parseInt(strExpectedResponseCode);
+      assertEquals ( expectedResponseCode, actualResponseCode );
+    }
+    
+    @Then("I expect a single training record with id matching {string} as response")
+    public void i_expect_a_single_training_record_with_id_matching_as_response(String strId) {
+
+      Training training = response.body().as(Training.class);
+
+      int expectedId = Integer.parseInt(strId);
+      int actualId = training.getId();
+
+      assertEquals( expectedId, actualId );
+
     }
 
     @Then("I expect all the training records from training table as response")
     public void i_expect_all_the_training_records_from_training_table_as_response() {
-    //    assertEquals ( 200, actualResponseCode );
+      
+      Training[] listOfTrainings = response.body().as(Training[].class);
+
+      int actualCount = listOfTrainings.length;
+
+      Training training = null;
+      for ( int index = 0; index < actualCount; ++index ) {
+          training = listOfTrainings[index];
+          assertEquals ( index+1, training.getId() );
+          assertFalse  ( training.getName().isEmpty() );
+          assertFalse  ( training.getDuration().isEmpty() );
+      }
+
+    }
+
+    @Then("I expect the name of the training to be {string}")
+    public void i_expect_the_name_of_the_training_to_be(String name) {
+      Training training = response.body().as(Training.class);
+
+      String expectedTrainingName = name;
+      String actualTrainingName = training.getName();
+
+      assertEquals( expectedTrainingName, actualTrainingName );
+    }
+
+    @Then("I expect the duration of the training to be {string}")
+    public void i_expect_the_duration_of_the_training_to_be(String duration) {
+      Training training = response.body().as(Training.class);
+
+      String expectedTrainingDuration = duration;
+      String actualTrainingDuration = training.getDuration();
+
+      assertEquals( expectedTrainingDuration, actualTrainingDuration );
     }
 
 }
--- Day3/CRUD-REST-API/src/test/java/org/tektutor/Training.feature
+++ Day3/CRUD-REST-API/src/test/java/org/tektutor/Training.feature
@@ -2,7 +2,16 @@ Feature: Develop a REST API that supports CRUD operations.
 
    Scenario: Should be able to retrieve all training records
    
-    Given there is a REST API that supports retrieve all training records
-     When I invoked the url
-     Then I expect all the training records from training table as response
-    
+     Given there is a REST API that supports retrieve all training records at "http://localhost:8080/tektutor-crud-api-1.0.0/rest/trainings/all"
+     When I invoked the REST endpoint with GET method
+     Then I expect the response code as "200"
+     And I expect all the training records from training table as response
+   
+   Scenario: Should be able to retrieve a specific training record
+   
+     Given there is a REST API that supports retrieving a specific training record at "http://localhost:8080/tektutor-crud-api-1.0.0/rest/trainings/1"
+     When I invoked the REST endpoint with GET method
+     Then I expect the response code as "200"
+     And  I expect a single training record with id matching "1" as response
+     And  I expect the name of the training to be "Docker"
+     And I expect the duration of the training to be "2 days"
